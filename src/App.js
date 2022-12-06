928ae86ca9bac26c82b18f6da04cc387875f4bd1
amolmane7471 <maneamol3233@gmail.com>
2022-12-06 09:56:50 +0530

    [Amol]: added UC5 to display success or error msg on screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import PayrollForm from './components/payroll-form/PayrollForm'
-import Home from './components/home/Home';
 import Header from './components/Header';
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <Router>
         <Switch>
           <Route path="/register"><PayrollForm/></Route>
-          <Route path="/"><Home /></Route>
         </Switch>
       </Router>
       
--- src/service/EmployeeService.js
+++ src/service/EmployeeService.js
@@ -3,9 +3,23 @@ import axios from 'axios';
 class EmployeeService{
     baseUrl ="http://localhost:8081/employeeservice";
 
-    async addEmployee(data) {
+    addEmployee(data) {
         return axios.post(`${this.baseUrl}/create`, data);
     }
+
+    getAllEmployees() {
+        return axios.get(`${this.baseUrl}/get`);
+    }
+
+    getEmployeeById(employeeId) {
+        return axios.get(`${this.baseUrl}/get/${employeeId}`);
+    }
+
+
+    deleteEmployee(employeeId) {
+        return axios.delete(`${this.baseUrl}/delete/${employeeId}`);
+      }
+
 }
 
 export default new EmployeeService();
